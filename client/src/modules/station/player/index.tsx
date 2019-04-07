40a7cbd9dbf0f67c8c5bf680fb24850759035ec8
Khanh Ly <khanh.bao.ly@mgm-tp.com>
2019-04-07 16:55:23 +0700

    feat: Add react-player dependency

--- client/src/modules/station/player/index.tsx
+++ client/src/modules/station/player/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Typography, Card } from '@material-ui/core';
 import { useStyles } from './styles';
+import ReactPlayer from 'react-player';
 
 export const Player: React.FC<{}> = () => {
   const classes = useStyles();
@@ -8,6 +9,8 @@ export const Player: React.FC<{}> = () => {
   return (
     <Card className={classes.container}>
       <Typography variant="subtitle1">Player</Typography>
+
+      <ReactPlayer url="https://www.youtube.com/watch?v=ysz5S6PUM-U" playing />
     </Card>
   );
 };
